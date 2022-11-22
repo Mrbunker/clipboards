b36b0a6566d210eb021f7a64bc950ea52a4c81f4
mrbunker <Mrbunker@qq.com>
2022-11-22 23:35:21 +0800

    feat: fill the empty content of the stateList

--- src/components/InputList.vue
+++ src/components/InputList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { ref } from "vue";
+  import { computed, ref } from "vue";
   import { onKeyStroke, useStorage } from "@vueuse/core";
 
   interface StateItem {
@@ -14,18 +14,28 @@
     { content: "", id: 3 },
     { content: "", id: 4 },
   ];
+
   const initStateStr = JSON.stringify(initState);
 
   const StateList = useStorage<StateItem[]>("Datalist", JSON.parse(initStateStr));
+  /** StateList 中，第一个 content 为空的下标 */
+  const emptyIndex = computed(() => {
+    return StateList.value.findIndex((item) => {
+      if (item.content === "") return item;
+    });
+  });
 
-  const handleAdd = async (str?: string) => {
+  const handlePaste = async (str?: string) => {
     const clipText = await navigator.clipboard.readText();
-    const newItem: StateItem = {
-      content: str ?? clipText,
-      id: StateList.value[StateList.value.length - 1].id + 1,
-    };
-    StateList.value.push(newItem);
-
+    if (emptyIndex.value !== -1) {
+      StateList.value[emptyIndex.value].content = clipText;
+    } else {
+      const newItem: StateItem = {
+        content: str ?? clipText,
+        id: StateList.value[StateList.value.length - 1].id + 1,
+      };
+      StateList.value.push(newItem);
+    }
     // 添加后不允许 undo
     if (clearBtn.value.btnState === "undo") clearBtn.value.btnState = "clear";
   };
@@ -58,7 +68,7 @@
   onKeyStroke(["v", "V"], (e) => {
     if (!e.ctrlKey) return;
     e.preventDefault();
-    handleAdd();
+    handlePaste();
   });
 </script>
 <template>
@@ -83,7 +93,7 @@
   </div>
 
   <div class="fixed right-28 bottom-10 flex justify-center content-center">
-    <button class="btn btn-primary rounded-md" @click="handleAdd()">add</button>
+    <button class="btn btn-primary rounded-md" @click="handlePaste()">paste</button>
     <button class="btn btn-primary rounded-md mx-2" @click="handleClearBtnClick(clearBtn.btnState)">
       {{ clearBtn.btnState }}
     </button>
