070a15ec0b76eb60c2859dadb2f86b9768153265
mrbunker <Mrbunker@qq.com>
2022-12-18 22:25:56 +0800

    disable undo after input

--- src/components/InputList.vue
+++ src/components/InputList.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { computed, ref } from "vue";
-  import { onKeyStroke, useStorage } from "@vueuse/core";
+  import { onKeyStroke, useActiveElement, useStorage } from "@vueuse/core";
 
   interface StateItem {
     content: string;
@@ -14,10 +14,9 @@
     { content: "", id: 3 },
     { content: "", id: 4 },
   ];
-
   const initStateStr = JSON.stringify(initState);
-
   const StateList = useStorage<StateItem[]>("Datalist", JSON.parse(initStateStr));
+
   /** StateList 中，第一个 content 为空的下标 */
   const emptyIndex = computed(() => {
     return StateList.value.findIndex((item) => {
@@ -25,8 +24,27 @@
     });
   });
 
+  const currentCopyIndex = useStorage<number>("currentCopyIndex", 0);
+
+  // !todo settings.json
+  const settings = useStorage<{ trim: boolean; quick: boolean }>(
+    "settings",
+    JSON.parse(
+      JSON.stringify({
+        trim: true,
+        quick: true,
+      }),
+    ),
+  );
+
+  const getFormatText = (text: string) => {
+    if (settings.value.trim) return text.trim();
+    else return text;
+  };
+
   const handlePaste = async (str?: string) => {
-    const clipText = await navigator.clipboard.readText();
+    const clipText = getFormatText(await navigator.clipboard.readText());
+    // 如果有空的输入框，就插入到空输入框中，否则新开一个
     if (emptyIndex.value !== -1) {
       StateList.value[emptyIndex.value].content = clipText;
     } else {
@@ -36,16 +54,23 @@
       };
       StateList.value.push(newItem);
     }
-    // 添加后不允许 undo
-    if (clearBtn.value.btnState === "undo") clearBtn.value.btnState = "clear";
   };
 
-  const handleCopyBtnClick = (item: StateItem) => {
+  const handleCopyItem = (item: StateItem) => {
     navigator.clipboard.writeText(item.content);
+  };
+
+  const handleCopy = () => {
+    currentCopyIndex.value = currentCopyIndex.value + 1;
   };
 
   const handleDelectBtnClick = (index: number) => {
     StateList.value.splice(index, 1);
+  };
+
+  const handleInputChange = () => {
+    // 完成新的输入后不允许 undo clear
+    if (clearBtn.value.btnState === "undo") clearBtn.value.btnState = "clear";
   };
 
   const clearBtn = ref<{ btnState: "clear" | "undo"; saveThelastState: StateItem[] }>({
@@ -66,6 +91,13 @@
 
   /** 当按下 ctrl+v 时 */
   onKeyStroke(["v", "V"], (e) => {
+    // 完成新的输入后不允许 undo clear
+    if (clearBtn.value.btnState === "undo") clearBtn.value.btnState = "clear";
+
+    // 正在聚焦输入框时，不触发粘贴快捷键
+    const activeElement = useActiveElement();
+    if (activeElement.value?.dataset.type === "input") return;
+
     if (!e.ctrlKey) return;
     e.preventDefault();
     handlePaste();
@@ -79,11 +111,12 @@
           type="text"
           placeholder="Type your text here"
           class="input input-bordered w-full max-w-xs rounded-md rounded-r-none"
+          data-type="input"
+          @input="handleInputChange()"
+          spellcheck="false"
           v-model="item.content"
         />
-        <button class="btn rounded-md rounded-l-none" @click="handleCopyBtnClick(item)">
-          copy
-        </button>
+        <button class="btn rounded-md rounded-l-none" @click="handleCopyItem(item)">copy</button>
         <button class="btn rounded-md mx-2" @click="handleDelectBtnClick(index)">
           <!-- prettier-ignore -->
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" > <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /> </svg>
@@ -92,9 +125,10 @@
     </div>
   </div>
 
-  <div class="fixed right-28 bottom-10 flex justify-center content-center">
-    <button class="btn btn-primary rounded-md" @click="handlePaste()">paste</button>
-    <button class="btn btn-primary rounded-md mx-2" @click="handleClearBtnClick(clearBtn.btnState)">
+  <div class="fixed right-10 bottom-10 flex justify-center content-center">
+    <button class="btn btn-primary rounded-md mr-2" @click="handleCopy">copy</button>
+    <button class="btn btn-primary rounded-md mr-2" @click="handlePaste()">paste</button>
+    <button class="btn btn-primary rounded-md" @click="handleClearBtnClick(clearBtn.btnState)">
       {{ clearBtn.btnState }}
     </button>
   </div>
